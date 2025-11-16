5f8c91f8a22a7d2a35aef263e1760c8f0d05e5c4
SuK014 <sukonlap@gmail.com>
2025-11-16 15:02:57 +0700

    feat/deployRequirement

--- server/server.py
+++ server/server.py
@@ -773,3 +773,8 @@ async def private_challenge_responseV2(sid, data):
 
     if challenger_id in active_games:
         del active_games[challenger_id]
+        
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
